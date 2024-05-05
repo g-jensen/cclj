c62c1f623d54511eb27ea82ea4ffa56cc74b5a5c
g-jensen <greg.micah.jensen@gmail.com>
2024-05-05 01:00:06 -0400

    destroy values when done

--- spec/eval_spec.c
+++ spec/eval_spec.c
@@ -6,35 +6,71 @@ module(eval_spec, {
   
   describe("eval", {
     it("blank strings", {
-      should(value_eq(value_create_nil(),eval("")));
-      should(value_eq(value_create_nil(),eval(" ")));
-      should(value_eq(value_create_nil(),eval("  ")));
-      should(value_eq(value_create_nil(),eval("\n")));
-      should(value_eq(value_create_nil(),eval(" \n ")));
+      Value nil = value_create_nil();
+      Value v = eval("");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      v = eval(" ");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      v = eval("  ");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      v = eval("\n");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      v = eval(" \n ");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      value_destroy(nil);
     });
 
     it("\"nil\"", {
-      should(value_eq(value_create_nil(),eval("nil")));
+      Value nil = value_create_nil();
+      Value v = eval("nil");
+      should(value_eq(nil,v));
+      value_destroy(v);
+      value_destroy(nil);
     });
 
     it("\"false\"", {
-      should(value_eq(value_create_false(),eval("false")));
+      Value false = value_create_false();
+      Value v = eval("false");
+      should(value_eq(false,v));
+      value_destroy(v);
+      value_destroy(false);
     });
 
     it("\"true\"", {
-      should(value_eq(value_create_true(),eval("true")));
+      Value true = value_create_true();
+      Value v = eval("true");
+      should(value_eq(true,v));
+      value_destroy(v);
+      value_destroy(true);
     });
 
     it("\"0\"", {
-      should(value_eq(value_create_long(0),eval("0")));
+      Value zero = value_create_long(0);
+      Value v = eval("0");
+      should(value_eq(zero,v));
+      value_destroy(v);
+      value_destroy(zero);
     });
 
     it("\"1\"", {
-      should(value_eq(value_create_long(1),eval("1")));
+      Value one = value_create_long(1);
+      Value v = eval("1");
+      should(value_eq(one,v));
+      value_destroy(v);
+      value_destroy(one);
     });
 
     it("\"123\"", {
-      should(value_eq(value_create_long(123),eval("123")));
+      Value onetwothree = value_create_long(123);
+      Value v = eval("123");
+      should(value_eq(onetwothree,v));
+      value_destroy(v);
+      value_destroy(onetwothree);
     });
   });
 });
